e91457d95098e90b8e4b7ca6040d93ab56a4d436
Alper Çıtak <citak.alper@gmail.com>
2022-07-02 12:43:36 +0300

    added endpoint: /api/users

---
+++ pages/api/users.ts
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const users = await prisma.users.findMany();
+  res.status(200).json(users);
+}
